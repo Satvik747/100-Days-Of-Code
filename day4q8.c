61cc3fe47ca3c6bddcf9b6099dec73e60210221e
Satvik Arora <satvik.arora8a@gmail.com>
2025-08-26 20:23:52 +0530

    Add files via upload

---
+++ day4q8.c
@@ -0,0 +1,23 @@
+/*Q8: Write a program to find and display the sum of the first n natural numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a natural number: ");
+    scanf("%d",&n);
+   printf("The sum of the first %d natural numbers is %d\n", n, n*(n+1)/2 );
+    return 0;
+}
---
+++ day5q9.c
@@ -0,0 +1,27 @@
+/*Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 5 2
+Output 1:
+Simple Interest=100, Compound Interest=102.5
+
+Input 2:
+5000 7 3
+Output 2:
+Simple Interest=1050, Compound Interest=1125.76
+
+*/
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int p,r,t;
+    float si,ci;
+    printf("Enter principal, rate and time: ");
+    scanf("%d %d %d",&p,&r,&t);
+    si=(p*r*t)/100.0;   
+    ci=p*(pow((1+r/100.0),t)-1);
+    printf("Simple Interest=%.2f, Compound Interest=%.2f\n",si,ci);
+    return 0;
+}
