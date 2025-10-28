3b1f3a603f1aa915d551a15b32fd5661b8fa4e3d
Satvik Arora <satvik.arora8a@gmail.com>
2025-10-28 15:36:01 +0530

    Commit

--- DAY_36_A_PRINT_MATRIX.c
+++ DAY_36_A_PRINT_MATRIX.c
@@ -17,12 +17,9 @@ int main() {
     int rows, cols;
     int matrix[10][10];
     int i, j;
-
-    // Read number of rows and columns
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &rows, &cols);
 
-    // Read matrix elements
     printf("Enter elements of the matrix:\n");
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
@@ -30,15 +27,15 @@ int main() {
         }
     }
 
-    // Print matrix
     printf("\nThe entered matrix is:\n");
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            printf("%4d ", matrix[i][j]); // formatted for neat spacing
+            printf("%4d ", matrix[i][j]); 
         }
         printf("\n");
     }
 
     return 0;
 }
+
 
